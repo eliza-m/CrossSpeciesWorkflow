b39bf2b675f5af7bb70cea45e0bc8783cda27e6d
Anton Kulaga <antonkulaga@gmail.com>
2020-08-02 03:03:15 +0300

    fixed species_proteins container

--- dockerfiles/species_proteins/README.md
+++ dockerfiles/species_proteins/README.md
@@ -1,2 +1,6 @@
+## species_proteins docker container ##
+
 species_proteins docker container is for the scripts involved in the workflow.
+Right now I only added a script to load fasta sequences.
 It is quite complex container as it uses conda and it also has docker context outside of its folder.
+Its building context is base folder of the project.
--- species_proteins/workflow/run.py
+++ species_proteins/workflow/run.py
@@ -1,5 +1,19 @@
+import click
 from bioservices import *
-u = UniProt()
-f = u.get_fasta("P32321")
-print("Test fasta print!")
-print(f)
+from bioservices.apps import FASTA
+
+@click.command()
+@click.option('--uniprot', required=True,  help='Id of the protein to fetch')
+@click.option('--filename', required=False, help='filename to save')
+def get_fasta(uniprot: str, filename: str = None):
+    """Simple program that greets NAME for a total of COUNT times."""
+    click.echo(f"receiving fasta sequence for uniprot id ${uniprot}")
+    f: FASTA = FASTA()
+    f.get_fasta(uniprot) # bioservices use ugly mutable getters
+    output = uniprot + ".fasta" if filename is None else (filename if ".fasta" in filename else filename+".fasta")
+    click.echo(f"saving protein fasta to ${output}")
+    f.save_fasta(output)
+    return output
+
+if __name__ == '__main__':
+    get_fasta()
